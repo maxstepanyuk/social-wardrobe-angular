25d4b7e58eb5a5343722e0508aab909daf2c66b6
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-11-15 22:43:32 +0200

    wip: sinup form - fields, styles, temp logic

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
+  //password
+  newPass: string = "";
+  hideNewPass: boolean = true;
+  //todo: validation
+  newPassWalid() {
+    return "todo"
+  }
+  getNewPassErrorMessage() {
+    return "todo"
+  }
+  newPassInvalid(): any {
+    return "todo"
+  }
+  confiremPass: string = "";
+
+  arePasswordsMatch(): any {
+    return (this.newPass === this.confiremPass && this.newPass != "");
+  }
 }
