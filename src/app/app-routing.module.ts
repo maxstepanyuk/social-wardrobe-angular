1a390589e67a7e9e005059515c4ad639571c9d4d
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-11-15 10:27:59 +0200

    added login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
   {path: "**", component: HomeComponent},
 ];
 
