e3bb1d48dadac2a6fcc419db2037c7e0a57797f8
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-11-15 15:22:47 +0200

    wip: login form - styles, logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     FormsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Enter a valid email';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  //password
+  hidePass: boolean = true;
+
+  //todo: validation
 }
