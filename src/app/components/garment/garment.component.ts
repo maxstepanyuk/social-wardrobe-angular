7266c72a7fa3d2e0e2f8dbc274aa4760e498e4ea
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-11-17 22:44:18 +0200

    adited styles and date logic

--- src/app/components/garment/garment.component.ts
+++ src/app/components/garment/garment.component.ts
@@ -8,4 +8,11 @@ import { Garment } from './garment'
 })
 export class GarmentComponent {
   @Input() garment!: Garment;
+  
+  calculateDaysSinceLastWorn(lastWornDate: Date): number {
+    const today = new Date();
+    const timeDifference = today.getTime() - lastWornDate.getTime();
+    const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+    return daysDifference;
+  }
 }
