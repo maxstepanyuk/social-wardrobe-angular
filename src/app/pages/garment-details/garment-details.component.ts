5e3a776a26718c6dfb2c8027b6230cdd26a54dc6
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-11-18 02:16:01 +0200

    implemented ActivatedRoute

--- src/app/pages/garment-details/garment-details.component.ts
+++ src/app/pages/garment-details/garment-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-garment-details',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./garment-details.component.scss']
 })
 export class GarmentDetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  garmentId = 0;
 
+  constructor() {
+    this.garmentId = Number(this.route.snapshot.params['id'])
+  }
 }
