057a77e93ab85860424e13d8878903f4cb972fa1
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-01 22:52:02 +0200

    wip - outfit html

--- src/app/components/outfit/outfit.component.ts
+++ src/app/components/outfit/outfit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Outfit } from './outfit';
 
 @Component({
   selector: 'app-outfit',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./outfit.component.scss']
 })
 export class OutfitComponent {
-
+  // @Input() outfit!: Outfit;
+  outfit: Outfit = { //use input later
+    id: 1,
+    name: "Casual Chic",
+    img: "https://picsum.photos/200/300?image=1042",
+    type: "Casual",
+    date: new Date("2023-10-04"),
+    garments: [
+      {
+        id: 1,
+        name: "Red Shirt",
+        img: "https://picsum.photos/200/300?image=1027",
+        type: "Top",
+        date: new Date("2023-10-04"),
+      },
+      {
+        id: 2,
+        name: "Blue Jeans",
+        img: "https://picsum.photos/200/300?image=1028",
+        type: "Bottom",
+        date: new Date("2023-10-05"),
+      },
+      {
+        id: 5,
+        name: "White Sneakers",
+        img: "https://picsum.photos/200/300?image=1000",
+        type: "Shoes",
+        date: new Date("2023-10-08"),
+      },
+    ],
+  };
 }
