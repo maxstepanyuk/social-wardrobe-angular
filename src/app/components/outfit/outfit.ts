105b9b81759c775c7099fcb333af34dee0b816de
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-01 21:51:35 +0200

    added new outfits temp and made img optional

--- src/app/components/outfit/outfit.ts
+++ src/app/components/outfit/outfit.ts
@@ -3,7 +3,7 @@ import { Garment } from "../garment/garment";
 export interface Outfit {
     id: number,
     name: string,
-    img: string,
+    img?: string,
     type: string,
     date: Date,
     // garmentsId: number[]; //TODO later??
