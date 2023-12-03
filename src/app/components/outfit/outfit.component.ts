b211752b686f5f3da2785ab0602929a9fcb3a698
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-04 00:28:49 +0200

    added @Input to the outfit

--- src/app/components/outfit/outfit.component.ts
+++ src/app/components/outfit/outfit.component.ts
@@ -9,8 +9,7 @@ import { DateUtilsService } from '../../servises/date-utils.service';
 })
 export class OutfitComponent {
   garmentService = inject(DateUtilsService);
-  //TODO
-  // @Input() outfit!: Outfit;
+  @Input() outfit!: Outfit;
 
   constructor() {
     //create a daysAgo=calculateDaysSinceLastWorn() field??
@@ -19,35 +18,4 @@ export class OutfitComponent {
   calculateDaysSinceLastWorn(lastWornDate: Date): number {
     return this.garmentService.calculateDaysSinceLastWorn(lastWornDate);
   }
-
-  outfit: Outfit = { //use input later
-    id: 1,
-    name: "Casual Chic",
-    img: "https://picsum.photos/200/300?image=1042",
-    type: "Casual",
-    date: new Date("2023-10-04"),
-    garments: [
-      {
-        id: 1,
-        name: "Red Shirt",
-        img: "https://picsum.photos/200/300?image=1027",
-        type: "Top",
-        date: new Date("2023-10-04"),
-      },
-      {
-        id: 2,
-        name: "Blue Jeans",
-        img: "https://picsum.photos/200/300?image=1028",
-        type: "Bottom",
-        date: new Date("2023-10-05"),
-      },
-      {
-        id: 5,
-        name: "White Sneakers",
-        img: "https://picsum.photos/200/300?image=1000",
-        type: "Shoes",
-        date: new Date("2023-10-08"),
-      },
-    ],
-  };
 }
