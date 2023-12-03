8f3dce29b8b16d669701b9157a98b9103774519b
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-03 16:43:32 +0200

    made maethod not ststic

--- src/app/servises/date-utils.service.ts
+++ src/app/servises/date-utils.service.ts
@@ -7,7 +7,7 @@ export class DateUtilsService {
 
   constructor() { }
   
-  static calculateDaysSinceLastWorn(lastWornDate: Date): number {
+  calculateDaysSinceLastWorn(lastWornDate: Date): number {
     const today = new Date();
     const timeDifference = today.getTime() - lastWornDate.getTime();
     const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
