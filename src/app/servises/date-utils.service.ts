6237c3ada89b40a543e85ccaacdb67bd19fad783
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-03 16:31:53 +0200

    added calculateDaysSinceLastWorn to DateUtilsService

--- src/app/servises/date-utils.service.ts
+++ src/app/servises/date-utils.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class DateUtilsService {
 
   constructor() { }
+  
+  static calculateDaysSinceLastWorn(lastWornDate: Date): number {
+    const today = new Date();
+    const timeDifference = today.getTime() - lastWornDate.getTime();
+    const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+    return daysDifference;
+  }
 }
