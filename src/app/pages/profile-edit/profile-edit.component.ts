12a4ab4d0a74128d1d3c353a844cd542a16b3ad9
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-08 11:58:21 +0200

    added pages/profile-edit

--- src/app/pages/profile-edit/profile-edit.component.ts
+++ src/app/pages/profile-edit/profile-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/components/user-table/user';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-edit',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class ProfileEditComponent {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  newImage: string | undefined;
+
+  constructor() {
+    this.newImage = undefined;
+
+  }
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  handleFileInput(event: any): void {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        this.newImage = e.target?.result as string;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  //password
+  newPass: string = "";
+  hideNewPass: boolean = true;
+  //todo: validation
+  newPassWalid() {
+    return "todo"
+  }
+  getNewPassErrorMessage() {
+    return "todo"
+  }
+  newPassInvalid(): any {
+    return "todo"
+  }
+  confiremPass: string = "";
+
+  arePasswordsMatch(): any {
+    return (this.newPass === this.confiremPass && this.newPass != "");
+  }
 }
