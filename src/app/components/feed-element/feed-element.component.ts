96397f37e2340b7e722323042d5d79aee5be523e
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-09 23:27:47 +0200

    created feed-element component (shows outfits inside it)

--- src/app/components/feed-element/feed-element.component.ts
+++ src/app/components/feed-element/feed-element.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Outfit } from '../outfit/outfit';
+import { Garment } from '../garment/garment';
+
+enum PostTypes {
+  Other = 1,
+  Outfit,
+  Garment,
+}
 
 @Component({
   selector: 'app-feed-element',
@@ -6,5 +14,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./feed-element.component.scss']
 })
 export class FeedElementComponent {
+
+    
+  // @Input() post!: Outfit | Garment;
+  @Input() post!: Outfit;
+
+
+  // //TODO - DE
+  // postTypes = PostTypes;
+
+  // //TOD - check??
+  // postType = PostTypes.Outfit;
+
+  username: string;
+  subtitle: string;
+  
+  constructor() {
+    //TODO - get from @Input, use interface
+    this.username = "user1";
+    this.subtitle = "added";
+
+    // if (PostTypes.Garment === this.postType) {
+    //   this.subtitle += " garmant.";
+    // } else if (PostTypes.Outfit === this.postType) {
+    //   this.subtitle += " outfit.";
+    // }
+
+
+  }
 
 }
