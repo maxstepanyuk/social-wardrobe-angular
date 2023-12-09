a973c6213cac7e56d9323aa5acea209be1ad38e2
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2023-12-09 23:29:47 +0200

    created feed page

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Outfit } from 'src/app/components/outfit/outfit';
+import { OutfitService } from '../../servises/outfit.service';
+
 
 @Component({
   selector: 'app-feed',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
+  outfitList: Outfit[] = [];
+  outfitService: OutfitService = inject(OutfitService)
 
+  constructor(){
+    this.outfitList = this.outfitService.getAllOutfits();
+  }
 }
