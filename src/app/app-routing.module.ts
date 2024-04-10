b2f6e544000255eed2278d79b83e7c3a9136e28d
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2024-04-10 15:15:47 +0300

    upd - configured provideRouter or RouterModule.forRoot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ROUTER_CONFIGURATION } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
@@ -21,7 +21,6 @@ const routes: Routes = [
   //TODO rename `details` to `clothes` 
   { path: "details", component: GarmentDetailsComponent }, //TODO garment create NEW
   { path: "details/:id", component: GarmentDetailsComponent }, //garment get, edit(TODO), delete(TODO)
-  
   { path: "outfits", component: OutfitsComponent }, //get all outfit
   { path: "outfit/:id", component: OutfitCreatorComponent }, //TODO outfit get(TODO), edit(TODO), delete(TODO)
   { path: "creator", component: OutfitCreatorComponent }, //outfit create
@@ -34,6 +33,17 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  //for similar behavior in Angular version 17 like in 16.
+  //providers array can be omitted altogether
+  providers: [ 
+    { provide: ROUTER_CONFIGURATION, useValue: {  
+        paramsInheritanceStrategy: 'always',
+        urlUpdateStrategy: 'deferred',
+        urlHandlingStrategy: 'path',
+        titleStrategy: 'default',
+        canceledNavigationResolution: 'completed'
+    } }
+  ]
 })
 export class AppRoutingModule { }
