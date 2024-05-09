663ef3a8041636a4bb0c60da8e87ad8a7c66a771
Maks Step - Deb pc <40894200+maxstepanyuk@users.noreply.github.com>
2024-05-09 21:05:22 +0300

    added environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: "YOUR_DEV_URL_HERE",
+    supabaseKey: "YOUR_DEV_KEY_HERE"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: "YOUR_PROD_URL_HERE",
+    supabaseKey: "YOUR_PROD_KEY_HERE"
+};
