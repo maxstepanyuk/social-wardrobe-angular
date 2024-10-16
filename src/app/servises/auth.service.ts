6bf36aa7fcf64c44b5aae18d3b0269ada59145a7
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2024-10-17 01:47:12 +0300

    refactored - delegate Supabase client creation to SupabaseService (og)

--- src/app/servises/auth.service.ts
+++ src/app/servises/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { SupabaseClient, User, createClient } from '@supabase/supabase-js';
+import { SupabaseClient, User } from '@supabase/supabase-js';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { SupabaseService } from './supabase.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,47 +12,39 @@ export class AuthService {
   private _currentUser: BehaviorSubject<boolean | User | any> = new BehaviorSubject(null);
 
   constructor(
-    private router: Router
+    private router: Router,
+    private supabaseService: SupabaseService
   ) {
-    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+    this.supabase = this.supabaseService.getClient();
 
-    // // const user = this.supabase.auth.user();
-    // const { data, error } = await this.supabase.auth.signInWithOtp({
-    //   email: 'example@email.com',
-    //   options: {
-    //     emailRedirectTo: 'https://example.com/welcome'
-    //   }
-    // })
-    const user = this.supabase.auth.getUser(); //todo - add the 'await' or is this ok? no - handle a promise
-
-    console.log('user ', user)
-
-    if (user) {
-      this._currentUser.next(user);
-    } else {
-      this._currentUser.next(false);
-    }
+    this.initializeUser();
 
     this.supabase.auth.onAuthStateChange((event, session) => {
       console.log('event ', event);
       console.log('session ', session);
 
       if (event === 'SIGNED_IN') {
-        this._currentUser.next(session?.user)
+        this._currentUser.next(session?.user);
       } else {
-        this._currentUser.next(false)
-        this.router.navigateByUrl(
-          '/login',
-          // {replaceUrl: true}s
-        )
+        this._currentUser.next(false);
+        this.router.navigateByUrl('/login');
       }
-    })
+    });
+  }
 
+  private async initializeUser() {
+    const { data: user } = await this.supabase.auth.getUser();
+    console.log('user ', user);
+
+    if (user) {
+      this._currentUser.next(user);
+    } else {
+      this._currentUser.next(false);
+    }
   }
 
   signInWithEmail(email: string) {
-    // used to be     return this.supabase.auth.signIn({email})
-    return this.supabase.auth.signInWithOtp({ email: email, options: {} }); //TODO if this isnt ok
+    return this.supabase.auth.signInWithOtp({ email: email, options: {} });
   }
 
   logOut() {
@@ -63,6 +55,3 @@ export class AuthService {
     return this._currentUser.asObservable();
   }
 }
-
-// https://supabase.com/docs/reference/javascript/auth-api - Sign in a user through OTP
-// https://www.youtube.com/watch?v=jmCiI_OSarA
