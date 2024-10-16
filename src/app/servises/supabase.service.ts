6483388b4749b74f2b0e4cb2ca2bf97ebdb2fce0
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2024-10-17 00:47:22 +0300

    integrated Supabase client in SupabaseService (og)

--- src/app/servises/supabase.service.ts
+++ src/app/servises/supabase.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupabaseService {
+  private supabase: SupabaseClient;
 
-  constructor() { }
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+  getClient(): SupabaseClient {
+    return this.supabase;
+  }
 }
