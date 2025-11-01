8aeca25055e362a24b482446241209d3baf8ef94
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-01 22:28:59 +0200

    `SignupComponent` use `authService.postUser` and use `snackBar`

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
   Validators,
   AbstractControl,
@@ -6,6 +6,10 @@ import {
   FormBuilder,
   FormGroup,
 } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/servises/auth.service';
+import { UserCreate, UserResponse } from 'src/app/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -15,10 +19,18 @@ import {
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup;
+  router = inject(Router);
   hideNewPass: boolean = true;
   hideConfirmPass: boolean = true;
+  errorMessage: string = '';
+  successMessage: string = '';
 
-  constructor(private fb: FormBuilder) {
+  private authService: AuthService = inject(AuthService);
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
     this.signupForm = this.fb.group(
       {
         email: ['', [Validators.required, Validators.email]],
@@ -116,11 +128,43 @@ export class SignupComponent implements OnInit {
     return '';
   }
 
+  prepareUser(): UserCreate {
+    const { email, username, password } = this.signupForm.value;
+    return {
+      login: username,
+      email: email,
+      password: password,
+    }
+  }
+
   onSubmit() {
+    this.errorMessage = '';
+    this.successMessage = '';
+
     if (this.signupForm.valid) {
-      // TODO
-      console.log('Form submitted:', this.signupForm.value);
-      alert('Form submitted');
+
+      this.signupForm.disable();
+      const userData = this.prepareUser();
+
+      this.authService.postUser(userData).subscribe({
+        next: (response: UserResponse) => {
+          // console.log(response)
+          this.successMessage = 'Account created successfully!';
+          const bar = this.snackBar.open(this.successMessage, 'Login');
+          bar.onAction().subscribe(() => {this.router.navigate(['/login']);})
+          this.signupForm.reset();
+          this.signupForm.enable();
+        },
+        error: (error) => {
+          this.isLoading = false;
+          
+          this.signupForm.enable();
+          this.errorMessage = error.error?.detail || 'An error occurred during signup. Please try again.';
+          this.snackBar.open(this.errorMessage, 'Close');
+          // console.error('Signup error:', error);
+        }
+      });
+
     } else {
       Object.keys(this.signupForm.controls).forEach((key) => {
         this.signupForm.get(key)?.markAsTouched();
