091176c2b838fc9acc52a6858411cc9e1395a5a6
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-01 22:27:48 +0200

    add `postUser` to `AuthService` with `apiUrl`

--- src/app/servises/auth.service.ts
+++ src/app/servises/auth.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { UserCreate, UserResponse } from '../models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+
+  http = inject(HttpClient);
+  apiUrl: string;
+
+  constructor() {
+    this.apiUrl = "http://127.0.0.1:8000/"
+  }
+
+  postUser(user: UserCreate): Observable<UserResponse> {
+    return this.http.post<UserResponse>(this.apiUrl + "users/", user)
+  }
 }
