4a1d5809ec0c9311240ac3d758c1d10be18f9c49
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-01 19:31:29 +0200

    add interfaces `UserCreate`, `UserResponse`, `UserLoginEmailPass` in `user.ts`

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,3 +4,31 @@ export interface UserOld {
   bio: string;
   img: string;
 }
+
+export interface UserCreate {
+  login: string;
+  email: string;
+
+  password: string;
+
+  name?: string;
+  image_link?: string;
+  image_id?: number;
+}
+
+export interface UserResponse {
+  login: string;
+  email: string;
+
+  name?: string;
+  image_link?: string;
+  image_id?: number;
+
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface UserLoginEmailPass {
+  email: string;
+  password: string;
+}
