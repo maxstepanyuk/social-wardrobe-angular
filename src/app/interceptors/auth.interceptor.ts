ac61efc60595f2f6bad360e80c239a788cf821c9
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-03 22:53:22 +0200

    `authInterceptor` add `AuthService.getAuthorizationHeader` if it is available

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../servises/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const authHeader = authService.getAuthorizationHeader();
+
+  if (authHeader) {
+    const clonedRequest = req.clone({
+      headers: req.headers.set('Authorization', authHeader)
+    });
+    return next(clonedRequest);
+  }
+
   return next(req);
 };
