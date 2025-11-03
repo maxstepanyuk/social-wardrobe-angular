5db58de31d5da5a041f928845be14c8bcd110b21
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-03 17:38:25 +0200

    `LoginComponent` - check inputs, submit logic, Reactive Form, use `authService.UserLoginEmailPass`, upd its html template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,24 +1,103 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/servises/auth.service';
+import { UserLoginEmailPass } from 'src/app/models/user';
+import { Router } from '@angular/router';
+import { Token } from 'src/app/models/token';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: false
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: false
 })
-export class LoginComponent {
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-  getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'Enter a value';
-    }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  //password
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  router = inject(Router);
   hidePass: boolean = true;
 
-  //todo: validation
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  private authService: AuthService = inject(AuthService);
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
+    this.loginForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]],
+      },
+
+    );
+  }
+
+  ngOnInit() { }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  getEmailErrorMessage() {
+    if (this.email?.hasError('required')) {
+      return 'Email is required';
+    }
+    return this.email?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password?.hasError('required')) {
+      return 'Password is required';
+    }
+    return '';
+  }
+
+  prepareUser(): UserLoginEmailPass {
+    const { email, password } = this.loginForm.value;
+    return {
+      email: email,
+      password: password,
+    }
+  }
+
+
+  onSubmit() {
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    if (this.loginForm.valid) {
+
+      this.loginForm.disable();
+      const userData: UserLoginEmailPass = this.prepareUser();
+
+      this.authService.authWithEmailPass(userData)
+        .subscribe({
+          next: (response: Token) => {
+            // console.log(response)
+            this.successMessage = 'Login successful!';
+            this.snackBar.open(this.successMessage, 'Close', { duration: 5000 });
+            this.loginForm.reset();
+            this.loginForm.enable();
+            this.router.navigate(['/outfits']);
+          },
+          error: (error) => {
+            // console.error('Signup error:', error);
+            this.loginForm.enable();
+            this.errorMessage = error.error?.detail || 'An error occurred during login. Please try again.';
+            this.snackBar.open(this.errorMessage, 'Close');
+          }
+        });
+
+    } else {
+      Object.keys(this.loginForm.controls).forEach((key) => {
+        this.loginForm.get(key)?.markAsTouched();
+      });
+    }
+  }
 }
