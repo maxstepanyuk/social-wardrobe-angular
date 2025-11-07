6215e8ebdbb944b85d06e8d43cb03c7b7f2d2962
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-07 22:36:01 +0200

    `ClothesComponent` page used `getAllGarmentsObservable()` and the new `app-garment-card`

--- src/app/pages/clothes/clothes.component.ts
+++ src/app/pages/clothes/clothes.component.ts
@@ -1,20 +1,29 @@
 import { Component, inject } from '@angular/core';
 // import { Garment } from 'src/app/components/garment/garment';
-import { GarmentOld } from '../../models/garment';
+import { GarmentOld, GarmentResponse } from '../../models/garment';
 import { GarmentService } from '../../servises/garment.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-    selector: 'app-clothes',
-    templateUrl: './clothes.component.html',
-    styleUrls: ['./clothes.component.scss'],
-    standalone: false
+  selector: 'app-clothes',
+  templateUrl: './clothes.component.html',
+  styleUrls: ['./clothes.component.scss'],
+  standalone: false
 })
 export class ClothesComponent {
-  garmentList: GarmentOld[] = [];
+  garmentList: GarmentResponse[] = [];
   garmentService: GarmentService = inject(GarmentService)
 
-  constructor(){
-    this.garmentList = this.garmentService.getAllGarments();
+  constructor(
+    private snackBar: MatSnackBar,
+  ) {
+    this.garmentService.getAllGarmentsObservable().subscribe({
+      next: (response) => {this.garmentList = response;},
+      error: (error) => {
+        this.snackBar.open(error.error?.detail || 'An error occurred. Please try again.', 'Close');
+        // console.error(error);
+      }
+    })
   }
 
   applyFilter(event: Event) {
