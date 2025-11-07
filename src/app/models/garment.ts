87e41d54df2970adb48729109491203fe2eef77c
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-07 22:18:59 +0200

    add `GarmentCreate` and `GarmentResponse` interfaces

--- src/app/models/garment.ts
+++ src/app/models/garment.ts
@@ -5,4 +5,43 @@ export interface GarmentOld {
     type: string,
     date: Date,
 }
-//TODO add userId
+
+export interface GarmentCreate {
+    name?: string;
+    description?: string;
+    image_link?: string;
+    last_worn?: Date;
+
+    gender_id?: number;
+    category_master_id?: number;
+    category_sub_id?: number;
+    type_id?: number;
+    color_id?: number;
+    season_id?: number;
+    usage_id?: number;
+
+    hex?: string;
+}
+
+export interface GarmentResponse {
+    id: number;
+    user_id: number;
+
+    created_at: Date;
+    updated_at: Date;
+
+    name?: string;
+    description?: string;
+    image_link?: string;
+    last_worn?: Date;
+
+    gender_id?: number;
+    category_master_id?: number;
+    category_sub_id?: number;
+    garment_type_id?: number;
+    color_id?: number;
+    season_id?: number;
+    usage_id?: number;
+
+    hex?: string;
+}
