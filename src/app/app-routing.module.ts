78f73975a6ba6f46346e68e8731f260e2c7afb06
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-14 23:02:39 +0200

    change `details` `routerLink`-s to `/garments/editor/` AND used `GarmentEditorComponent` in the routes instead of `GarmentDetailsComponent`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,22 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ClothesComponent } from './pages/clothes/clothes.component';
 import { OutfitsComponent } from './pages/outfits/outfits.component';
-import { GarmentDetailsComponent } from './pages/garment-details/garment-details.component';
+// import { GarmentDetailsComponent } from './pages/garment-details/garment-details.component'; //old
 import { OutfitCreatorComponent } from './pages/outfit-creator/outfit-creator.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SearchComponent } from './pages/search/search.component';
 import { ProfileEditComponent } from './pages/profile-edit/profile-edit.component';
 import { FeedComponent } from './pages/feed/feed.component';
+import { GarmentEditorComponent } from './pages/garment-editor/garment-editor.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent }, //landing
   { path: "login", component: LoginComponent }, //todo logic
   { path: "signup", component: SignupComponent }, //todo logic
   { path: "clothes", component: ClothesComponent }, //get all clothes(garments)
-  //TODO rename `details` to `clothes` OR `garments`
-  { path: "details", component: GarmentDetailsComponent }, //TODO garment create NEW
-  { path: "details/:id", component: GarmentDetailsComponent }, //garment get, edit(TODO), delete(TODO)
+  { path: "garments/editor", component: GarmentEditorComponent },
+  { path: "garments/editor/:id", component: GarmentEditorComponent },
   { path: "outfits", component: OutfitsComponent }, //get all outfit
   { path: "outfit/:id", component: OutfitCreatorComponent }, //TODO outfit get(TODO), edit(TODO), delete(TODO)
   { path: "creator", component: OutfitCreatorComponent }, //outfit create
@@ -36,14 +36,16 @@ const routes: Routes = [
   exports: [RouterModule],
   //for similar behavior in Angular version 17 like in 16.
   //providers array can be omitted altogether
-  providers: [ 
-    { provide: ROUTER_CONFIGURATION, useValue: {  
+  providers: [
+    {
+      provide: ROUTER_CONFIGURATION, useValue: {
         paramsInheritanceStrategy: 'always',
         urlUpdateStrategy: 'deferred',
         urlHandlingStrategy: 'path',
         titleStrategy: 'default',
         canceledNavigationResolution: 'completed'
-    } }
+      }
+    }
   ]
 })
 export class AppRoutingModule { }
