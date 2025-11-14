db1bf004e38f448a0beb22e17ceb2516fc02e446
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-14 19:36:44 +0200

    add `image.ts` with `ImageInfoResponse` interface

---
+++ src/app/models/image.ts
@@ -0,0 +1,10 @@
+export interface ImageInfoResponse {
+    filename_store: string;
+    filename_original: string;
+
+    id: number;
+    user_id: number;
+
+    created_at: Date;
+    createupdated_at: Date;
+}
