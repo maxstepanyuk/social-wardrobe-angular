421cf3cedff13df7bee3cca193a015154c1e5c05
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-14 23:08:29 +0200

    `ClothesComponent` use `imageService.getImageLink`

--- src/app/pages/clothes/clothes.component.ts
+++ src/app/pages/clothes/clothes.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { GarmentOld, GarmentResponse } from '../../models/garment';
 import { GarmentService } from '../../servises/garment.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ImageService } from 'src/app/servises/image.service';
 
 @Component({
   selector: 'app-clothes',
@@ -13,12 +14,20 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ClothesComponent {
   garmentList: GarmentResponse[] = [];
   garmentService: GarmentService = inject(GarmentService)
+  imageService = inject(ImageService);
 
   constructor(
     private snackBar: MatSnackBar,
   ) {
     this.garmentService.getAllGarmentsObservable().subscribe({
-      next: (response) => {this.garmentList = response;},
+      next: (response) => {
+        this.garmentList = response;
+        response.forEach(element => {
+          if (element.image_link) {
+            element.image_link = this.imageService.getImageLink(element.image_link)
+          }
+        });
+      },
       error: (error) => {
         this.snackBar.open(error.error?.detail || 'An error occurred. Please try again.', 'Close');
         // console.error(error);
