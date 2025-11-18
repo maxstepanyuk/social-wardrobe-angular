c86b6be66efeaa712334bcfb3996f5622740a699
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2025-11-18 10:27:12 +0200

    add `models/categories.ts` with `interface CategoryMasterResponse` and `interface CategorySubResponse`

---
+++ src/app/models/category.ts
@@ -0,0 +1,15 @@
+export interface CategoryMasterResponse {
+    id: number;
+    name?: string;
+    description?: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface CategorySubResponse {
+    id: number;
+    name?: string;
+    description?: string;
+    created_at: Date;
+    updated_at: Date;
+}
